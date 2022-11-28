a55f8629fcdc2236ae8680785c52fd6df396688d
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2022-11-28 18:41:43 +0500

    fix back_to_future

--- my_project_part_1/back_to_future/models.py
+++ my_project_part_1/back_to_future/models.py
@@ -1,10 +1,19 @@
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
 # TODO добавьте необходимый код в данный модуль
 class Code(models.Model):
     code = models.CharField(max_length=4)
+
+
+def check_code(value):
+    if not Code.objects.filter(code=value).exists():
+        raise ValidationError(
+            '%(value)s is not exists',
+            params={'value': value},
+        )
 
 
 class Vacation(models.Model):
