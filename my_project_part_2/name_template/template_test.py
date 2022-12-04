8715f0801e9b04e0ac0e59a4bf5c985e2f843db3
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2022-12-04 10:01:38 +0500

    fix name_template

--- my_project_part_2/name_template/template_test.py
+++ my_project_part_2/name_template/template_test.py
@@ -4,4 +4,11 @@ import pytest
 @pytest.mark.django_db
 def test_save_choco(client, name, status_code):
     # TODO напишите Ваш код здесь
-    pass
+    data = {
+        "name": name,
+        "description": "test address",
+        "price": 100
+    }
+
+    response = client.post("/chocolate/create/", data=data)
+    assert response.status_code == status_code
