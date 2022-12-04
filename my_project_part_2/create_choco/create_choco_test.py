38325ce427757087a2088da69c4ead6ce20de757
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2022-12-04 09:55:38 +0500

    fix healthcheck

--- my_project_part_2/create_choco/create_choco_test.py
+++ my_project_part_2/create_choco/create_choco_test.py
@@ -4,4 +4,17 @@ import pytest
 @pytest.mark.django_db
 def test_create_choco(client):
     # TODO напишите Ваш тест здесь
-    pass
+    expected_response = {
+        "name": "Choco"
+    }
+
+    data = {
+        "name": "Choco",
+        "description": "test",
+        "price": 22
+    }
+
+    response = client.post("/choco_create/", data=data)
+    assert response.status_code == 201
+    assert response.data == expected_response
+
